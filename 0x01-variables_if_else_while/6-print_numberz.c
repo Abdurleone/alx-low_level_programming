43966387d62572da16b0fc54d5f76eb30bbc0ee6
Abdurleone <abyyyusuf@gmail.com>
2023-02-16 23:25:13 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print single number in base 10
+ * no char and no printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	putchar(a + '0');
+	putchar('\n');
+	return (0);
+}
