aad048579edeb218ed217abb18682c0ea6c5e262
Abdurleone <abyyyusuf@gmail.com>
2023-02-21 10:10:01 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Print the sign of a number
+ * @n: Type int n
+ * Return: 0, 1, -1
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
