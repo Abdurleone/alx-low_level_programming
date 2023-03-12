cbae102ae9887d4c40886116d82c29389de9db77
Abdurleone <abyyyusuf@gmail.com>
2023-03-12 10:39:04 -0700

    -strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,16 +1,14 @@
 #include "main.h"
- /**
+/**
  * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void	
+ * * @dest: input value
+ * * @src: input value
+ * *  Return: void
  */
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -23,9 +21,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
-
-
+	}
 	dest[i] = '\0';
 	return (dest);
 }
