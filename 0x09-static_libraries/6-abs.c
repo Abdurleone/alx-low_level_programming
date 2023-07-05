d34a06bce5c9e3fec4d4858c456ad744cc13d689
abdurleone <abyyyusuf@gmail.com>
2023-07-05 13:12:57 +0300

    final

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the abosulte value of an integer
+ * @n: the int to check
+ * Return: the aboslute value of int
+*/
+
+int _abs(int n)
+{
+    if (n >= 0)
+    {
+        return (n);
+    }
+    return (-n);
+}
---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *strcat(char *dest, char *src);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+#endif
