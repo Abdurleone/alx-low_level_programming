4d6512af42d5fbac4a891a34b3daaa993d72c4c5
Abdurleone <abyyyusuf@gmail.com>
2023-08-03 03:30:51 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int total, power;
-    int len;
+	int i;
+	unsigned int dec_val = 0;
 
-    if (b == NULL)
-    return (0);
+	if (!b)
+		return (0);
 
-    for (len = 0; b[len]; len++)
-    {
-        if (b[len] != '0' && b[len] = '1')
-        return (0);
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-    for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-    {
-        if (b[len] = '1')
-        total += power;
-    }
-
-    return (total);
+	return (dec_val);
 }
