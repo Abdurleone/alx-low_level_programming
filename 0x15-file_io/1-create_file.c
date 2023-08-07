c6d625ceb8b3b0bae64e6495621f4039a6b7b0f9
Abdurleone <abyyyusuf@gmail.com>
2023-08-07 07:02:20 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(file, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
-	if(fd == -1 || w == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
 	close(fd);
