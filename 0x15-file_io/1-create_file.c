5c2a307d969fd9e1e9973b10b86f3e1155798404
Abdurleone <abyyyusuf@gmail.com>
2023-08-07 17:07:10 +0300

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +22,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(file, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(file, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(fd);
+	close(o);
 
 	return (1);
 }
