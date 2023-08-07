c40c200183db37c37701530fb018a07c10ed4336
Abdurleone <abyyyusuf@gmail.com>
2023-08-07 06:51:51 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	if(fd == -1)
+	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
