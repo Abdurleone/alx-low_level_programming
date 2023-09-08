03c3365b42e557ce0a36485fef4e7c6d93e98e07
Abdurleone <abyyyusuf@gmail.com>
2023-09-08 16:29:05 +0300

    0x1A

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,14 +14,14 @@ hash_table_t *hash_node_create(unsigned long int size)
 
     ht = malloc(sizeof(hash_table_t));
     if (ht == NULL)
-          return (NULL);
+	    return (NULL);
 
     ht->size = size;
     ht->array = malloc(sizeof(hash_node_t *) * size);
     if (ht->array == NULL)
-             return (NULL);
+	    return (NULL);
     for (i = 0; i < size; i++)
-            ht->array[i] = NULL;
+	    ht->array[i] = NULL;
 
     return (ht);
 }
