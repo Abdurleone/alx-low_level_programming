f1769eeeffd3793c2aebe07087f514ee30fc3511
Abdurleone <abyyyusuf@gmail.com>
2023-09-08 18:37:53 +0300

    0x1A

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -16,7 +16,7 @@ void hash_table_delete(hash_table_t *ht)
         if (ht->array[i] != NULL)
         {
             node = ht->array[i];
-	    while (node != NULL)
+	        while (node != NULL)
             {
                 tmp = node->next;
                 free(node->key);
