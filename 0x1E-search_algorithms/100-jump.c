1c2b812d35ea0363e3bf7741ebdc239ff4c94408
Abdurleone <abyyyusuf@gmail.com>
2023-12-13 17:31:21 +0300

    0x1E

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,23 +12,23 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-    size_t i = 0, n, jp;
+	size_t i = 0, n, jp;
 
-    if (!array || size == 0)
-    return (-1);
+	if (!array || size == 0)
+		return (-1);
 
-    for (n = sqrt(size); i += n)
-    {
-        if (array[i] >= value)
-        break;
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-    }
-    printf("Value found between indexes [%lu] and [%lu]\n", i - n, i);
-    for (jp = i - n; jp < size && jp <= i; jp++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", jp, array[jp]);
-        if (array[jp] == value)
-        return (jp);
-    }
-    return (-1);
+	for (n = sqrt(size); i += n)
+	{
+		if (array[i] >= value)
+			break;
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", i - n, i);
+	for (jp = i - n; jp < size && jp <= i; jp++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", jp, array[jp]);
+		if (array[jp] == value)
+			return (jp);
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,31 +12,31 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t i, low, high;
+	size_t i, low, high;
 
-    if (!array)
-    return (-1);
-    for (low = 0, high = size - 1; high >= low;)
-    {
-        i = low + (((double)(high - low) / (array[high] - array[low])) *
-        (value - array[low]));
-        if (i < size)
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        else
-        {
-            printf("Value checked array[%ld] is out of range\n", i);
-            break;
-        }
-        if (array[i] == value)
-        return (i);
-        if (array[i] > value)
-        {
-            high = i - 1;
-        }
-        else
-        {
-            low = i + 1;
-        }
-    }
-    return (-1);
+	if (!array)
+		return (-1);
+	for (low = 0, high = size - 1; high >= low;)
+	{
+		i = low + (((double)(high - low) / (array[high] - array[low])) *
+				(value - array[low]));
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+		{
+			high = i - 1;
+		}
+		else
+		{
+			low = i + 1;
+		}
+	}
+	return (-1);
 }
